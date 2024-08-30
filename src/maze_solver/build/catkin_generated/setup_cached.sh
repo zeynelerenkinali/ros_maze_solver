fed406e0fdfb1869db4c326df38009a43036c996
zeynelerenkinali <ernknal@gmail.com>
2024-08-30 23:46:02 +0100

    robotcraft_program is finished

---
+++ src/maze_solver/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/eren/robotcraft_maze_solver/src/maze_solver/build/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/eren/robotcraft_maze_solver/src/maze_solver/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/eren/robotcraft_maze_solver/src/maze_solver/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/eren/robotcraft_maze_solver/src/maze_solver:$ROS_PACKAGE_PATH"
---
+++ src/maze_solver/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest
+# Build directory: /home/eren/robotcraft_maze_solver/src/maze_solver/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
